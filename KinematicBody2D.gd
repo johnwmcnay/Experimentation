8b216ead39f13dde6b6dc69ba199e521b8415142
John McNay <john.w.mcnay@gmail.com>
2020-11-16 18:19:03 -0600

    Experimenting with raycasting from sprite

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -21,13 +21,19 @@ var velocity = Vector2()
 
 func get_input():
 	velocity = Vector2()
+	var ray = $RayCast2D2
+	
 	if Input.is_action_pressed('ui_right'):
+		ray.set_cast_to(Vector2( 50, 0 ))
 		velocity.x += 1
 	if Input.is_action_pressed('ui_left'):
+		ray.set_cast_to(Vector2( -50, 0 ))
 		velocity.x -= 1
 	if Input.is_action_pressed('ui_down'):
+		ray.set_cast_to(Vector2( 0, 50 ))
 		velocity.y += 1
 	if Input.is_action_pressed('ui_up'):
+		ray.set_cast_to(Vector2( 0, -50 ))
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
@@ -48,3 +54,9 @@ func _physics_process(delta):
 	get_input()
 	get_animation()
 	velocity = move_and_slide(velocity)
+	if ($RayCast2D2.is_colliding()):
+		print($RayCast2D2.get_collider().a)
+
+
+func _on_Area2D_body_entered(body):
+	print("test")
